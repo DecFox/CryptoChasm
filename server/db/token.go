5486a27e8fcca55c80ca3469a7484803e07c3d23
Decfox <decfox@localhost.localdomain>
2022-01-25 14:23:16 +0530

    code refactor

--- server/db/token.go
+++ server/db/token.go
@@ -5,24 +5,23 @@ import (
 )
 
 type Bid struct {
-	BidPrice  int64     `json:"bidPrice" bson:"bidPrice"`
+	BidPrice  float64   `json:"bidPrice" bson:"bidPrice"`
 	Bidder    string    `json:"bidder" bson:"bidder"`
 	TimeOfBid time.Time `json:"timeOfBid" bson:"timeOfBid"`
 }
 
 type List struct {
-	ListPrice   int64     `json:"listPrice" bson:"listPrice"`
+	ListPrice   float64   `json:"listPrice" bson:"listPrice"`
 	ListingTime time.Time `json:"listingTime" bson:"listingTime"`
 }
 
 type Token struct {
 	TokenHash string    `json:"tokenHash" bson:"tokenHash"`
 	Listed    bool      `json:"listed" bson:"listed"`
-	Price     int64     `json:"price" bson:"price"`
 	Minter    string    `json:"minter" bson:"minter,omitempty"`
 	MintedOn  time.Time `json:"mintedOn" bson:"mintedOn"`
 	Owners    []string  `json:"owners" bson:"owners,omitempty"`
-	Royalty   []int64   `json:"royalty" bson:"royalty,omitempty"`
+	Royalty   []float64 `json:"royalty" bson:"royalty,omitempty"`
 	Bids      []Bid     `json:"bids" bson:"bids,omitempty"`
 	Listing   []List    `json:"listing" bson:"listing,omitempty"`
 }
