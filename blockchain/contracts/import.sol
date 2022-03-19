6dc43120b3dab32ce57b215016b3379ff62b985c
Adnan Hashmi <hashmi.adnan791@gmail.com>
2022-03-19 20:19:30 +0530

    opengsn integrated with paymaster and relayHub

---
+++ blockchain/contracts/import.sol
@@ -0,0 +1,3 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.5.0 <0.9.0;
+import "@opengsn/contracts/src/RelayHub.sol";
--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -1,15 +1,20 @@
-
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  
   // We get the contract to deploy
-  const ChasmNFT = await hre.ethers.getContractFactory("ChasmNFT");
-  const chasmNFT = await ChasmNFT.deploy();
+  const ChasmNFT = await hre.ethers.getContractFactory('ChasmNFT');
+  const chasmNFT = await ChasmNFT.deploy(
+    '0xd8735997ec01b6Fb2F9B05BbDa0f42a81141521A'
+  );
+
+  const Paymaster = await hre.ethers.getContractFactory('Paymaster');
+  const pm = await Paymaster.deploy();
 
   await chasmNFT.deployed();
+  await pm.deployed();
 
-  console.log("Chasm NFT deployed to:", chasmNFT.address);
+  console.log('Chasm NFT deployed to:', chasmNFT.address);
+  console.log('Paymaster deployed to:', pm.address);
 }
 
 main()
